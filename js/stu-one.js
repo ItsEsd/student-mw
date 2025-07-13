5afb08147afb5bb30a4512f56a8a5447f5b4db16
soubhikdas <d.soubhik@hotmail.com>
2025-07-13 17:25:50 +0530

    up up date .. update ⬆️

--- js/stu-one.js
+++ js/stu-one.js
@@ -361,7 +361,7 @@ function loadegames() {
 `;
 
   const leftLabel = document.createElement("span");
-  leftLabel.textContent = "E-Game | MASTROWALL";
+  leftLabel.textContent = "E-Games | MASTROWALL";
 
   const closeText = document.createElement("span");
   closeText.textContent = "Close";
