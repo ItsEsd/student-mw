50b0b4fe6409f759665b211e113e5ea93276a8a4
soubhikdas <d.soubhik@hotmail.com>
2026-02-04 11:50:08 +0530

    up up date .. update 🔼

--- js/wallcook.js
+++ js/wallcook.js
@@ -73,6 +73,9 @@ function showGistBox(url) {
     closeBtn.onclick = () => {
       box.style.display = "none";
       document.body.style.overflowY = "auto";
+      if (history.state !== "gistbox-open") {
+        history.replaceState(null, "");
+      }
     };
 
     const iframe = document.createElement("iframe");
@@ -84,6 +87,9 @@ function showGistBox(url) {
     box.appendChild(closeBtn);
     box.appendChild(iframe);
     document.body.appendChild(box);
+  }
+  if (history.state !== "gistbox-open") {
+    history.pushState("gistbox-open", "");
   }
 }
 
@@ -102,6 +108,14 @@ function updateFetchUrl(newUrl) {
     console.warn("iframe not ready or not found");
   }
 }
+
+window.addEventListener("popstate", (ev) => {
+  if (ev.state !== "gistbox-open") {
+    document.getElementById("gistbox").style.display = "none";
+    document.body.style.overflowY = "auto";
+    history.replaceState(null, "");
+  }
+});
 
 // Cookies
 
